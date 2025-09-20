c13105c5555e79fe6750169e80b2dd1619f9169b
leo <devmontypython@gmail.com>
2025-09-20 14:47:21 -0300

    TELA DE LOGIN (login.php)  --  Aqui na tela de login vou inserir um caminho para o arquivo de cadastro, onde será cadastrado o usuário. Esse caminho vai conter um caminho de URL. Então, fora do tag <form> mas dentro da tag <main>  coloco a tag <a> e no seu link vou passar o index.php mais a minha URL de ação mais um arquivo de registro.

--- views/login.php
+++ views/login.php
@@ -53,8 +53,11 @@
 
     
     <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+    
   </form>
+  <br>
+  <a href="index.php?action=register">Cadastre-se</a>
+
 </main>
 
 
