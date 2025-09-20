f098b6c9bdc13085529c36588598802e2ed7a8b3
leo <devmontypython@gmail.com>
2025-09-19 23:48:20 -0300

    BANCO DE DADOS  --  3) Arquivo responsável por fazer a primeira parte de procurar um usuário no banco de dados pelo email.

---
+++ models/user.php
@@ -0,0 +1,24 @@
+<?php
+
+// Inclui o arquivo 'database.php' que contém a classe Database para gerenciar a conexão com o banco de dados
+require_once 'models/database.php';
+
+// Definição da classe User, que representa as operações relacionadas aos usuários no sistema
+class User {
+
+     // Função para encontrar um usuário pelo email
+     public static function findByEmail($email){
+         // Obtém a conexão com o banco de dados
+         $conn = Database::getConnection();
+         
+         // Prepara uma consulta SQL para buscar o usuário pelo email
+         $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+         
+         // Executa a consulta com o email passado como parâmetro
+         $stmt->execute(['email' => $email]);
+         
+         // Retorna os dados do usuário encontrado como um array associativo
+         return $stmt->fetch(PDO::FETCH_ASSOC);
+     }
+
+    }
